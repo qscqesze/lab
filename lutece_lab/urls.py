2cc4bcf20c3a34ead6b513904acdaf1c0b5f37ad
qingjun <qingjun@bytedance.com>
2018-08-10 00:27:26 +0800

    update

--- lutece_lab/urls.py
+++ lutece_lab/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import view
  
 urlpatterns = [
+    url(r'^news.html', view.news),
     url(r'^$', view.hello),
 ]
--- lutece_lab/view.py
+++ lutece_lab/view.py
@@ -1,7 +1,10 @@
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
     context = {}
     context['title'] = 'lutece.lab'
     return render(request, 'index.html', context)
+
+def news(request):
+    return render(request, 'news.html')
